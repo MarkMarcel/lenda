8e13389142fbd335cdd415f9edb99c6bcaf7acfe
Mark <marcelmarkamoako@yahoo.com>
2020-01-24 16:30:03 +0000

    Initial setup with abstract View Builder

---
+++ app/src/main/java/com/ie/lenda/datastructures/ArrayDataStructure.kt
@@ -0,0 +1,6 @@
+package com.ie.lenda.datastructures
+
+class ArrayDataStructure(val size:Int) {
+    private var array = Array<Int>(size) { i -> 0}
+
+}
---
+++ app/src/main/java/com/ie/lenda/viewbuilders/DataStructureViewBuilder.kt
@@ -0,0 +1,8 @@
+package com.ie.lenda.viewbuilders
+
+import android.view.View
+
+interface DataStructureViewBuilder {
+    fun getDataStructureView():View
+    fun getDataStructureOperationsView():View
+}
