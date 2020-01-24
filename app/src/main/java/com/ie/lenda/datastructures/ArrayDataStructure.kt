a72c30e94b8bca505b4c7115698e8a879dfcfa44
Mark <marcelmarkamoako@yahoo.com>
2020-01-24 16:46:47 +0000

    Insert and Get methods for ArrayDataStructure

--- app/src/main/java/com/ie/lenda/datastructures/ArrayDataStructure.kt
+++ app/src/main/java/com/ie/lenda/datastructures/ArrayDataStructure.kt
@@ -1,6 +1,13 @@
 package com.ie.lenda.datastructures
 
-class ArrayDataStructure(val size:Int) {
-    private var array = Array<Int>(size) { i -> 0}
+class ArrayDataStructure(size:Int) {
+    private var array = Array(size) { 0}
 
+    fun insert(value:Int, index:Int){
+        array[index] = value
+    }
+
+    fun get(index:Int):Int{
+        return array[index]
+    }
 }
--- app/src/test/java/com/ie/lenda/datastructurestest/ArrayDataStructureTest.kt
+++ app/src/test/java/com/ie/lenda/datastructurestest/ArrayDataStructureTest.kt
@@ -1,2 +1,23 @@
 package com.ie.lenda.datastructurestest
 
+import com.ie.lenda.datastructures.ArrayDataStructure
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.*
+
+class ArrayDataStructureTest {
+    private lateinit var array: ArrayDataStructure
+
+    @Before
+    fun createArray(){
+        this.array = ArrayDataStructure(10)
+    }
+    @Test
+    fun addsItemAtIndex(){
+        val value = 2
+        val index = 2
+
+        array.insert(value,index)
+        assertEquals(value,array.get(index))
+    }
+}
